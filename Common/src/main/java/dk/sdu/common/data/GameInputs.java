4d85a05f43bb9322311d2d25916753461ab13e29
Andreas528 <andreas.runge528@gmail.com>
2025-05-28 13:03:10 +0200

    Cleaned up GameInputs

--- Common/src/main/java/dk/sdu/common/data/GameInputs.java
+++ Common/src/main/java/dk/sdu/common/data/GameInputs.java
@@ -6,16 +6,11 @@ import java.util.Map;
 
 public class GameInputs {
     private static Map<EGameInputs, Boolean> keys;
-    private static Map<EGameInputs, Boolean> pkeys;
 
     public GameInputs() {
         keys = Collections.synchronizedMap(new EnumMap<>(EGameInputs.class));
-        pkeys = Collections.synchronizedMap(new EnumMap<>(EGameInputs.class));
     }
 
-    public void update() {
-        pkeys.putAll(keys);
-    }
 
     public void setInput(EGameInputs key, boolean b) {
         keys.put(key, b);
@@ -25,9 +20,6 @@ public class GameInputs {
         return keys.getOrDefault(key, false);
     }
 
-    public boolean isPressed(EGameInputs key) {
-        return keys.getOrDefault(key, false) & pkeys.getOrDefault(key, false);
-    }
 
 
 }
