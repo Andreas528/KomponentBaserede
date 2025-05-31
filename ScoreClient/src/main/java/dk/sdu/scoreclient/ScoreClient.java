525a5e2e61b0358d93d351d55b97f64ad7811f1e
Andreas528 <andreas.runge528@gmail.com>
2025-05-31 14:52:16 +0200

    Finished Microservice

--- ScoreClient/src/main/java/dk/sdu/scoreclient/ScoreClient.java
+++ ScoreClient/src/main/java/dk/sdu/scoreclient/ScoreClient.java
@@ -11,6 +11,7 @@ public class ScoreClient implements IScoreSPI {
         this.restTemplate = new RestTemplate();
     }
 
+    // Sends a score increment to the scoring server, and logs the updated score.
     public void addScore(int value) {
         {
             // Sends the request.
@@ -19,7 +20,7 @@ public class ScoreClient implements IScoreSPI {
                     String.class
             );
 
-
+            // Parse and log the updated score returned by the server.
             System.out.println("Score now: " + Integer.parseInt(response));
         }
     }
